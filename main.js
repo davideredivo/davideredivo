47bcf39e2096cae6401b50606cc62b9d5bc38341
davideredivo <43120369+davideredivo@users.noreply.github.com>
2018-09-10 09:30:03 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+     $(".nav-link").hover(function(){
+        $(".nav-link").toggleClass("drop");
+    });
+    
+     $(".title").hover(function(){
+        $(".title").toggleClass("gg");
+    });
+    
+    $(".subtitlebl").hover(function(){
+        $(".subtitlebl").toggleClass("gg");
+    });
+    
+    $(".subtitle").hover(function(){
+        $(".subtitle").toggleClass("gn");
+    });
+    
+    
+    
+    $(".contacts").hover(function(){
+        $(".contacts").toggleClass("run");
+    });
+    
+    
+    
+});
